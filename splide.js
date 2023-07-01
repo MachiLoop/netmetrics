f5550d314e664de2f8ce0b125e4b522714d77723
hamidu sodiq <hamidusodiq2@gmail.com>
2023-07-01 20:14:37 +0100

    modified home page intro-section

--- splide.js
+++ splide.js
@@ -1,12 +1,24 @@
 // import Splide from "/splide-4.1.3";
 
 // document.addEventListener("DOMContentLoaded", function () {
-var splide = new Splide(".splide", {
+var splide = new Splide("#second-splide", {
   type: "loop",
   perPage: 1,
   autoplay: true,
   speed: 10,
   fade: true,
+  pagination: false,
 });
-splide.mount();
 // });
+
+splideIntro = new Splide("#first-splide", {
+  type: "loop",
+  autoplay: true,
+  speed: 5,
+  fade: true,
+  arrows: false,
+  pagination: false,
+});
+
+splide.mount();
+splideIntro.mount();
